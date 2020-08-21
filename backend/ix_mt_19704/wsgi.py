f63146ba85e5881045f4eba6ffb3963663b4451f
Crowdbotics <team@crowdbotics.com>
2020-08-21 15:18:51 +0000

    Update models

--- backend/ix_mt_19704/wsgi.py
+++ backend/ix_mt_19704/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ix_mt_19704.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ix_mt_19704.settings")
 
 application = get_wsgi_application()
